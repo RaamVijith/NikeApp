9599c913c5247c7ee675b472dd42e0c2c4427361
RaamVijith <raamvijith1@gmail.com>
2023-06-24 21:14:38 +0530

    24/06/2023

--- src/screens/ShopingCartScreen.jsx
+++ src/screens/ShopingCartScreen.jsx
@@ -12,10 +12,7 @@ const ShoppingCartTotals = () => {
     <View style={styles.totalsContainer}>
      
      
-       <View style={styles.row}>
-         <Text style={styles.text}>Delivery</Text>
-         <Text style={styles.text}> {deliveryFee} US$</Text>
-       </View>
+       
        </View>
        <View style={styles.row}>
          <Text style={styles.text}>Delivery</Text>
